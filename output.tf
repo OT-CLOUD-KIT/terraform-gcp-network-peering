01c971fd1e12ad3d7abbb4fd29f8ec90673c2151
shreya-jaiswal37 <shreya.jaiswal@opstree.com>
2025-04-14 15:45:38 +0530

    adding readme file

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "vpc_peering_connections" {
+  description = "List of VPC peering connections created"
   value = { for k, v in google_compute_network_peering.peering-1 : k => v.id }
 }
 
 output "reverse_peering_connections" {
+  description = "List of reverse VPC peering connections created"  
   value = { for k, v in google_compute_network_peering.reverse-peering : k => v.id }
 }

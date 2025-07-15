609de22246c99ab9d2b34c555561785abfbbf0e2
shreya-jaiswal37 <156057205+shreya-jaiswal37@users.noreply.github.com>
2025-07-15 22:38:45 +0530

    Update main.tf

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,8 @@
 module "vpc_peering" {
   source       = "./module"
   vpc_peerings = var.vpc_peerings
+  network      = var,network
+  peer_network = var.peer_network
 
   providers = {
     google.peer = google.peer
